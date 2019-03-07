aa16a3b6a07ab849707b4a90bfa07050ca72a49a
rgirodon <remy.girodon@gmail.com>
2019-03-07 10:50:21 -0500

    Set up adios page

---
+++ app/Http/Controllers/AdiosController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdiosController extends Controller
+{
+    public function index() {
+        
+        return view('adios');
+    }
+}
